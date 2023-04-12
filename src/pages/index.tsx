be99ec287ac155c24e389ad0c6d23bfa1e5efd93
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-12 03:31:48 +0200

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import Layout from "@/components/Layout/Layout";
+import MaxWIdth from "@/components/MaxWIdth";
 
 export default function index() {
   return (
     <Layout>
-      <div>
-        <div className="px-10 space-x-5">
-          <h1 className="text-6xl max-w-xl font-serif">
-            <span className="underline decoration-black decoration-4">Medium</span> is a place to write, reac and connect
-          </h1>
-          <h2>It's easy and free to post your thinking on any topic and connect with millions of readers</h2>
+      <MaxWIdth>
+        <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+          <div className="px-10 space-x-5">
+            <h1 className="text-6xl max-w-xl font-serif">
+              <span className="underline decoration-black decoration-4">Medium</span> is a place to write, reac and connect
+            </h1>
+            <h2>It's easy and free to post your thinking on any topic and connect with millions of readers</h2>
+          </div>
+          <img
+            src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+            alt="logo-medium"
+            className="hidden md:inline-flex h-32 lg:h-full"
+          />
         </div>
-        <img src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
-      </div>
+
+        {/* POSTS */}
+      </MaxWIdth>
     </Layout>
   );
 }
